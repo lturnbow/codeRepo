682e6015a1db4bdb90c965af55315a8ca42af3f4
lturnbow <43084338+lturnbow@users.noreply.github.com>
2019-03-12 00:02:24 -0700

    Add files via upload

---
+++ ArrayClass/element1.cpp
@@ -0,0 +1,79 @@
+/******************************************************************************/
+/*!
+\file   element1.cpp
+\author Lauren Turnbow
+\par    email: lauren.turnbow\@digipen.edu
+\par    DigiPen login: lauren.turnbow
+\par    Course: CS225
+\par    Assignment #4
+\date   11/6/2016
+\brief
+Implementation of element1 class
+*/
+/******************************************************************************/
+#include "element1.h"
+namespace CS225 {
+/******************************************************************************/
+/*!
+	\brief
+		Element1 constructor
+
+	\param _val
+		Value to be placed in element1
+*/
+/******************************************************************************/
+	Element1::Element1(int _val) : val(_val) { }
+
+/******************************************************************************/
+/*!
+	\brief
+		Element1 get function
+
+	\return
+		Value
+*/
+/******************************************************************************/
+	int Element1::Get() const
+	{
+		return val;
+	}
+
+/******************************************************************************/
+/*!
+	\brief
+		Set function
+
+	\param new_val
+		Set new value
+*/
+/******************************************************************************/
+	void Element1::Set(int new_val)
+	{
+		val = new_val;
+	}
+
+/******************************************************************************/
+/*!
+	\brief
+		Print function
+*/
+/******************************************************************************/
+	void Element1::Print() const
+	{
+		std::cout << "[" << Get() << "] ";
+	}
+
+/******************************************************************************/
+/*!
+	\brief
+		Clone function
+
+	\return
+		Returns an AbstractElement
+*/
+/******************************************************************************/
+	Element1::AbstractElement* Element1::Clone()
+	{
+		return new Element1(val);
+	}
+}
---
+++ ArrayClass/element2.cpp
@@ -0,0 +1,117 @@
+/******************************************************************************/
+/*!
+\file   element2.cpp
+\author Lauren Turnbow
+\par    email: lauren.turnbow\@digipen.edu
+\par    DigiPen login: lauren.turnbow
+\par    Course: CS225
+\par    Assignment #4
+\date   11/6/2016
+\brief
+Implementation of element2 class
+*/
+/******************************************************************************/
+#include "element2.h"
+namespace CS225 {
+/******************************************************************************/
+/*!
+	\brief
+		Element2 constructor
+
+	\param _val
+		Value to be set
+*/
+/******************************************************************************/
+	Element2::Element2(int _val) : p_val(new int(_val)) { }
+
+/******************************************************************************/
+/*!
+	\brief
+		Copy constructor
+
+	\param rhs
+		Const Element2 object
+*/
+/******************************************************************************/
+	Element2::Element2(const Element2& rhs) : AbstractElement(), p_val(new int(*rhs.p_val)) { }
+
+/******************************************************************************/
+/*!
+	\brief
+		Assignment operator
+
+	\param rhs
+		Const object to be assigned
+
+	\return
+		Returns reference to Element2
+*/
+/******************************************************************************/
+	Element2& Element2::operator=(const Element2& rhs) 	{
+		if (this != &rhs) {
+			delete p_val;
+			p_val = new int(*rhs.p_val);
+		}
+		return *this;
+	}
+
+/******************************************************************************/
+/*!
+	\brief
+		Get function
+
+	\return
+		Returns a value
+*/
+/******************************************************************************/
+	int Element2::Get() const {
+		return *p_val;
+	}
+
+/******************************************************************************/
+/*!
+	\brief
+		Print function
+*/
+/******************************************************************************/
+	void Element2::Print() const {
+		std::cout << "(" << *p_val << ") ";
+	}
+
+/******************************************************************************/
+/*!
+	\brief
+		Set function
+
+	\param new_val
+		Value to be set
+*/
+/******************************************************************************/
+	void Element2::Set(int new_val) {
+		*p_val = new_val;
+	}
+
+/******************************************************************************/
+/*!
+	\brief
+		Element2 destructor
+*/
+/******************************************************************************/
+	Element2::~Element2() {
+		delete p_val;
+	}
+
+/******************************************************************************/
+/*!
+	\brief
+		Clone function
+
+	\return
+		Returns an abstract element with with p_val
+*/
+/******************************************************************************/
+	Element2::AbstractElement* Element2::Clone()
+	{
+		return new Element2(*p_val);
+	}
+}
