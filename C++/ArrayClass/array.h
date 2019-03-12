aa778d657b3452685b62be4d1ffd46fd271f2237
lturnbow <43084338+lturnbow@users.noreply.github.com>
2019-03-12 00:41:38 -0700

    Update array.h

--- C++/ArrayClass/array.h
+++ C++/ArrayClass/array.h
@@ -2,11 +2,6 @@
 /*!
 \file   array.h
 \author Lauren Turnbow
-\par    email: lauren.turnbow\@digipen.edu
-\par    DigiPen login: lauren.turnbow
-\par    Course: CS225
-\par    Assignment #4
-\date   11/6/2016
 \brief
 Declaration of array class
 */
