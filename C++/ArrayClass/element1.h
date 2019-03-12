50d91b1e415ed1f1569888ba6ee5dd6f3588bcc1
lturnbow <43084338+lturnbow@users.noreply.github.com>
2019-03-12 00:42:25 -0700

    Update element1.h

--- C++/ArrayClass/element1.h
+++ C++/ArrayClass/element1.h
@@ -2,11 +2,6 @@
 /*!
 \file   element1.h
 \author Lauren Turnbow
-\par    email: lauren.turnbow\@digipen.edu
-\par    DigiPen login: lauren.turnbow
-\par    Course: CS225
-\par    Assignment #4
-\date   11/6/2016
 \brief
 Declaration of element1 class
 */
