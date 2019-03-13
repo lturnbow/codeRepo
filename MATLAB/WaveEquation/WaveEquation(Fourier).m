ac293c9b0dd894adb9f81deeb97f882457b94bd9
lturnbow <43084338+lturnbow@users.noreply.github.com>
2019-03-13 12:36:32 -0700

    Update WaveEquation(Fourier).m

--- MATLAB/WaveEquation/WaveEquation(Fourier).m
+++ MATLAB/WaveEquation/WaveEquation(Fourier).m
@@ -1,42 +1,47 @@
 % Lauren Turnbow
 % MAT363
-% Project 2
+% Project 1
 clear all
 clc
-a=2.1;         
-b=5.1; 
+a=2;         
+b=5;         
 c=0.02;      
 t=0:.5:300;      
 time=600;
-x=.1:(2/19):a;
+x=0:(2/19):a;
 p=length(x);
-y=linspace(.1,b,p);
+y=linspace(0,b,p);
 q=length(y);
-deltax=0.1;
-deltay=0.1;
-deltat=.3;
-r=c*deltat/deltax;
+deltax=(b-a)/p;
+deltay=(b-a)/q;
+AMatrix=zeros(20,20);
 
-for i=1:p %time = 0
-    for j=1:q
-        u(i,j,1)=3*(1-abs(sin(i*deltax)))*(1-abs(sin(2*j*deltay)))+2*(1-abs(sin(5*i*deltax)))*(1-abs(sin(7*j*deltay)));
-    end
-end
-
-for i=1:p %first time iteration
-    for j=1:q
-        u(i,j,2) = 3*(1-abs(sin(i*deltax)))*(1-abs(sin(2*j*deltay)))+2*(1-abs(sin(5*i*deltax)))*(1-abs(sin(7*j*deltay)))+deltat*sin((i*deltax+j*deltay)^2);
-    end
-end
-
-for k=2:time 
-     for i=2:p 
-         for j=2:q 
-            if (i == p || j == q) %to not go out of bounds
-                break;
+for m=1:20
+    for n=1:20
+        amnIntegral=0;
+        for i=1:p
+            for j=1:q
+                % f(x,y) = xy(cos(xy)+sin(xy))
+                amnIntegral=amnIntegral+(x(i)*y(j)*(cos(x(i)*y(j))+sin(x(i)*y(j))))*sin(m*pi*x(i)/a)*sin(n*pi*y(j)/b)*deltax*deltay;
             end
-            u(i,j,k+1)=r^2*(u(i+1,j,k)+u(i,j+1,k)+u(i-1,j,k)+u(i,j-1,k)-4*u(i,j,k))+2*u(i,j,k)-u(i,j,k-1)+(deltat^2)*(sin((i*deltax*j*deltay)^2+k));    
-         end
+        end
+        AMatrix(m,n)=(4/(a*b))*amnIntegral;
+        disp(m);
+    end
+end
+
+for k=1:time 
+     for i=1:p 
+         for j=1:q 
+            sum=0;
+            for m=1:20
+                for n=1:20
+                    kmn=pi*sqrt((m/a)^2+(n/b)^2);
+                    sum=sum+AMatrix(m,n)*cos(kmn*t(k)*c)*sin(m*pi*x(i)/a)*sin(n*pi*y(j)/b);
+                end
+            end
+            u(i,j,k)=sum;
+        end
     end
     disp(k)
 end
@@ -46,7 +51,7 @@ fig=figure;
 for i=1:time
     surf(X,Y,u(:,:,i));
     title('2D Wave Equation Simulation');
-    axis([0 a 0 b -30 30])
+    axis([0 a 0 b -10 10])
     xlabel('X Axis');
     ylabel('Y Axis');
     zlabel('Z Axis');
